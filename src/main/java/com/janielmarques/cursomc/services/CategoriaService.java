aded5c2a72bb510912d38e6c9d5e7197f6b4bee3
Janiel Marques <janielmarques@icloud.com>
2024-11-20 21:36:54 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/janielmarques/cursomc/services/CategoriaService.java
+++ src/main/java/com/janielmarques/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.janielmarques.cursomc.domain.Categoria;
 import com.janielmarques.cursomc.repositories.CategoriaRepository;
+import com.janielmarques.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,8 +17,8 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		
-	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 
 }
